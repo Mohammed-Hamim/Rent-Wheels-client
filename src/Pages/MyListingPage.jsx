a8e171c4e77b6836cbf06d5a28b34fbc918192fd
Mohammed- Hamim <mohammedhamim0158@gmail.com>
2025-11-11 02:29:43 +0600

    showing mylist data from db is done

--- src/Pages/MyListingPage.jsx
+++ src/Pages/MyListingPage.jsx
@@ -1,6 +1,24 @@
-import React from 'react';
+import React, { use, useEffect, useState } from 'react';
+import { AuthContext } from '../Provider/AuthProvider';
 
 const MyListingPage = () => {
+    const { user } = use(AuthContext)
+    const [myListing, setMyListing] = useState([])
+
+    useEffect(() => {
+
+        if (user?.email) {
+            fetch(`http://localhost:3000/all_cars?email=${user?.email} `)
+                .then(res => res.json())
+                .then(data => {
+                    console.log("Your bids", data)
+                    setMyListing(data)
+                })
+                .catch(err => console.log(err))
+        }
+
+    }, [user])
+
     return (
         <div className="min-h-screen bg-gray-900 text-gray-100 py-12">
             <div className="container mx-auto px-4 md:px-8 lg:px-16">
@@ -26,28 +44,31 @@ const MyListingPage = () => {
 
                         <tbody>
                             {/* Example Row */}
+                            {
+                                myListing.map(list =>
+                                    <tr key={list._id} className="border-b border-gray-700 hover:bg-gray-700/40 transition duration-200">
+                                        <td className="py-4 px-6">{list.car_name}</td>
+                                        <td className="py-4 px-6">{list.category}</td>
+                                        <td className="py-4 px-6">${list.rent_price} / day</td>
+                                        <td className="py-4 px-6">
+                                            <span className={`px-3 py-1 rounded-full text-sm font-medium ${list.status && "bg-red-400 text-white"} bg-green-300 text-gray-900`}>
+                                                {list.status ? `${list.status}`:"Available"}
+                                            </span>
+                                        </td>
+                                        <td className="py-4 px-6 text-center">
+                                            <div className="flex justify-center gap-3">
+                                                <button className="px-4 py-2 rounded-lg btn-outline border-2 border-teal-700  hover:bg-teal-300 transition font-semibold text-white">
+                                                    Update
+                                                </button>
+                                                <button className="px-4 py-2 rounded-lg btn-outline border-2 border-red-500  hover:bg-red-500 transition font-semibold text-white">
+                                                    Delete
+                                                </button>
+                                            </div>
+                                        </td>
+                                    </tr>
 
-
-                            <tr className="border-b border-gray-700 hover:bg-gray-700/40 transition duration-200">
-                                <td className="py-4 px-6">BMW X5</td>
-                                <td className="py-4 px-6">SUV</td>
-                                <td className="py-4 px-6">$120 / day</td>
-                                <td className="py-4 px-6">
-                                    <span className="px-3 py-1 rounded-full text-sm font-medium bg-red-300 text-white">
-                                        Booked
-                                    </span>
-                                </td>
-                                <td className="py-4 px-6 text-center">
-                                    <div className="flex justify-center gap-3">
-                                        <button className="px-4 py-2 rounded-lg btn-outline border-2 border-teal-700  hover:bg-teal-300 transition font-semibold text-white">
-                                            Update
-                                        </button>
-                                        <button className="px-4 py-2 rounded-lg btn-outline border-2 border-red-500  hover:bg-red-500 transition font-semibold text-white">
-                                            Delete
-                                        </button>
-                                    </div>
-                                </td>
-                            </tr>
+                                )
+                            }
                         </tbody>
                     </table>
                 </div>
